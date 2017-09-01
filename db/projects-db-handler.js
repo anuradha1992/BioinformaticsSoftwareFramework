ee5fd0a27517c6ec06c0c0366300474a3a71cd59
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2017-09-02 02:00:28 +0530

    added sample webhook and initated a db handler

---
+++ db/projects-db-handler.js
@@ -0,0 +1,9 @@
+/**
+ * Created by anuradhawick on 9/2/17.
+ */
+// TODO to be implemented after deciding on the db schema (Mongo Preferred)
+export default class ProjectsDBHandler {
+    constructor() {
+
+    }
+}
