61b0e9127bf370491f4d2b7f97295ccd13614ff5
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2017-09-03 21:32:57 +0530

    added biopython blast wrapper class with example

--- server.js
+++ server.js
@@ -12,69 +12,76 @@ fs.readFile(__dirname + '/public/json/webservices.json', 'utf8', function (err, 
     if (err) throw err;
     obj = JSON.parse(data);
     console.log(obj);
- });
+});
 
 app.get('/listServices', function (req, res) {
-  console.log(obj);
-  res.end(JSON.stringify(obj));
+    console.log(obj);
+    res.end(JSON.stringify(obj));
 })
 
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
 });
 
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
+router.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
 });
 
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
+router.get("/about", function (req, res) {
+    res.sendFile(path + "about.html");
 });
 
-router.get("/contact",function(req,res){
-  res.sendFile(path + "contact.html");
+router.get("/contact", function (req, res) {
+    res.sendFile(path + "contact.html");
 });
 
-router.get("/styles/styles.css",function(req,res){
-  res.sendFile("/styles/styles.css");
+router.get("/styles/styles.css", function (req, res) {
+    res.sendFile("/styles/styles.css");
 });
 
-app.use("/",router);
+app.use("/", router);
 app.use(express.static(__dirname + '/public/'));
 
-app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
+app.use("*", function (req, res) {
+    res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(3000, function () {
+    console.log("Live at Port 3000");
 });
+
+
+import WrapBlast from './python-wrappers/blast/wrap-blast';
+
+const wb = new WrapBlast('asd', 'asd', 'asda');
+wb.exec();
+
 
 /* ncbi blastp backend */
 
-var blast = require('blastjs');
-
-var type = 'nucl';
-var fileIn = './ncbi/test.fasta';
-var outPath = './ncbi';
-var name = 'test';
-
-blast.makeDB(type, fileIn, outPath, name, function(err){
-  if(err){
-    console.error(err);   
-  } else {
-    console.log('database created at', outPath);
-  }
-});
-
-blast.outputString(true); //optional
- 
-var dbPath = './ncbi/test';
-var query = 'CTAATACCGAATAAGGTCAGTTAATTTGTTAATTGATGAAAGGAAGCCTT';
- 
-blast.blastN(dbPath, query, function (err, output) {
-  if(!err){
-    console.log(output);
-  }
-});
+// var blast = require('blastjs');
+//
+// var type = 'nucl';
+// var fileIn = './ncbi/test.fasta';
+// var outPath = './ncbi';
+// var name = 'test';
+//
+// blast.makeDB(type, fileIn, outPath, name, function(err){
+//   if(err){
+//     console.error(err);
+//   } else {
+//     console.log('database created at', outPath);
+//   }
+// });
+//
+// blast.outputString(true); //optional
+//
+// var dbPath = './ncbi/test';
+// var query = 'CTAATACCGAATAAGGTCAGTTAATTTGTTAATTGATGAAAGGAAGCCTT';
+//
+// blast.blastN(dbPath, query, function (err, output) {
+//   if(!err){
+//     console.log(output);
+//   }
+// });
