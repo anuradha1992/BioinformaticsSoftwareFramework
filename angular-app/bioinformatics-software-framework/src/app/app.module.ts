5fc6a563d3be0fb94db98042eba67ccb51c5b71e
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2017-10-06 16:24:21 +0530

    refactoring project to UI and API components

--- angular-app/bioinformatics-software-framework/src/app/app.module.ts
+++ angular-app/bioinformatics-software-framework/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+const routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: '',
+    component: LoginPageComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
