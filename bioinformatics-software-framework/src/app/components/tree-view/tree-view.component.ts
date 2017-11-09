483290106cf5dd119d3dafef029e14ce59e177f8
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2017-11-09 20:53:09 +0530

    Remove unused components in tree view

--- bioinformatics-software-framework/src/app/components/tree-view/tree-view.component.ts
+++ bioinformatics-software-framework/src/app/components/tree-view/tree-view.component.ts
@@ -27,100 +27,100 @@ export class TreeViewComponent implements OnInit {
             }
           ]
         },
-        {
-          "text": "JDBC",
-          "children": [
-            {
-              "id": "J001",
-              "text": "Execute SQL Query",
-              "precedence": "1",
-              "isStep": true,
-              "inputs": [
-                {
-                  "name": "db",
-                  "type": "db",
-                  "value": ""
-                },
-                {
-                  "name": "retmode",
-                  "type": "text",
-                  "value": ""
-                }
-              ],
-              "OutputParams": {
-                "output": ""
-              },
-              "description": "Execute"
-            },
-            {
-              "Id": "J002",
-              "text": "Execute SQL Update",
-              "isStep": true,
-              "precedence": "1",
-              "inputs": [
-                {
-                  "name": "db",
-                  "type": "db",
-                  "value": ""
-                },
-                {
-                  "name": "retmode",
-                  "type": "text"
-                }
-              ],
-              "OutputParams": {
-                "output": ""
-              },
-              "description": "Execute an SQL update given the params"
-            }
-          ],
-          "description": "List of web services related to query processing"
-        },
-        {
-          "text": "General",
-          "children": [
-            {
-              "Id": "G001",
-              "text": "Split by *",
-              "isStep": true,
-              "precedence": "2",
-              "inputs": [
-                {
-                  "name": "input_report",
-                  "type": "db",
-                  "value": ""
-                },
-                {
-                  "name": "delimeter",
-                  "type": "db",
-                  "value": ""
-                }
-              ],
-              "OutputParams": {
-                "output_seq_list": ""
-              },
-              "description": "Execute"
-            },
-            {
-              "Id": "G002",
-              "text": "Sort",
-              "isStep": true,
-              "precedence": "3",
-              "inputs": [
-                {
-                  "name": "input_list",
-                  "type": "db",
-                  "value": ""
-                }
-              ],
-              "OutputParams": {
-                "output": ""
-              },
-              "description": "Execute an SQL update given the params"
-            }
-          ],
-          "description": "List of web services related to query processing"
-        },
+        // {
+        //   "text": "JDBC",
+        //   "children": [
+        //     {
+        //       "id": "J001",
+        //       "text": "Execute SQL Query",
+        //       "precedence": "1",
+        //       "isStep": true,
+        //       "inputs": [
+        //         {
+        //           "name": "db",
+        //           "type": "db",
+        //           "value": ""
+        //         },
+        //         {
+        //           "name": "retmode",
+        //           "type": "text",
+        //           "value": ""
+        //         }
+        //       ],
+        //       "OutputParams": {
+        //         "output": ""
+        //       },
+        //       "description": "Execute"
+        //     },
+        //     {
+        //       "Id": "J002",
+        //       "text": "Execute SQL Update",
+        //       "isStep": true,
+        //       "precedence": "1",
+        //       "inputs": [
+        //         {
+        //           "name": "db",
+        //           "type": "db",
+        //           "value": ""
+        //         },
+        //         {
+        //           "name": "retmode",
+        //           "type": "text"
+        //         }
+        //       ],
+        //       "OutputParams": {
+        //         "output": ""
+        //       },
+        //       "description": "Execute an SQL update given the params"
+        //     }
+        //   ],
+        //   "description": "List of web services related to query processing"
+        // },
+        // {
+        //   "text": "General",
+        //   "children": [
+        //     {
+        //       "Id": "G001",
+        //       "text": "Split by *",
+        //       "isStep": true,
+        //       "precedence": "2",
+        //       "inputs": [
+        //         {
+        //           "name": "input_report",
+        //           "type": "db",
+        //           "value": ""
+        //         },
+        //         {
+        //           "name": "delimeter",
+        //           "type": "db",
+        //           "value": ""
+        //         }
+        //       ],
+        //       "OutputParams": {
+        //         "output_seq_list": ""
+        //       },
+        //       "description": "Execute"
+        //     },
+        //     {
+        //       "Id": "G002",
+        //       "text": "Sort",
+        //       "isStep": true,
+        //       "precedence": "3",
+        //       "inputs": [
+        //         {
+        //           "name": "input_list",
+        //           "type": "db",
+        //           "value": ""
+        //         }
+        //       ],
+        //       "OutputParams": {
+        //         "output": ""
+        //       },
+        //       "description": "Execute an SQL update given the params"
+        //     }
+        //   ],
+        //   "description": "List of web services related to query processing"
+        // },
         {
           "text": "NCBI",
           "children": [
@@ -146,116 +146,116 @@ export class TreeViewComponent implements OnInit {
               },
               "description": "BlastN compares sequences by local alignment"
             },
-            {
-              "Id": "S002",
-              "text": "FASTA",
-              "precedence": "4",
-              "isStep": true,
-              "inputs": [
-                {
-                  "name": "db",
-                  "type": "db",
-                  "value": ""
-                },
-                {
-                  "name": "retmode",
-                  "type": "text",
-                  "value": ""
-                }
-              ],
-              "OutputParams": {
-                "output": ""
-              },
-              "description": "FASTA aligns DNA and protein sequences"
-            },
-            {
-              "Id": "S003",
-              "text": "GBSeq",
-              "precedence": "4",
-              "isStep": true,
-              "inputs": [
-                {
-                  "name": "db",
-                  "type": "db",
-                  "value": ""
-                },
-                {
-                  "name": "retmode",
-                  "type": "text",
-                  "value": ""
-                }
-              ],
-              "OutputParams": {
-                "output": ""
-              },
-              "description": "GBSeq represents the elements in a GenBank style report of a sequence with some small additions to structure and support for protein (GenPept) versions of GenBank format as seen in Entrez."
-            },
-            {
-              "Id": "S004",
-              "text": "blast_ddbj",
-              "precedence": "4",
-              "isStep": true,
-              "inputs": [
-                {
-                  "name": "query_seq",
-                  "type": "seq",
-                  "value": ""
-                },
-                {
-                  "name": "database",
-                  "type": "db",
-                  "value": ""
-                },
-                {
-                  "name": "program",
-                  "type": "text",
-                  "value": ""
-                }
-              ],
-              "OutputParams": {
-                "text_blast_out": ""
-              },
-              "description": "BLAST finds regions of similarity between biological sequences. Uses the DDBJ’s UniProt BLAST service that forms part of its Web API for Bioinformatics pages."
-            },
-            {
-              "Id": "S005",
-              "text": "blast simplifier",
-              "precedence": "4",
-              "isStep": true,
-              "inputs": [
-                {
-                  "name": "gi_number",
-                  "type": "int",
-                  "value": ""
-                },
-                {
-                  "name": "input_seq",
-                  "type": "seq",
-                  "value": ""
-                }
-              ],
-              "OutputParams": {
-                "simplified_report": ""
-              },
-              "description": "BLAST finds regions of similarity between biological sequences. Uses the DDBJ’s UniProt BLAST service that forms part of its Web API for Bioinformatics pages."
-            },
-            {
-              "Id": "S006",
-              "text": "Get_Protein_FASTA",
-              "precedence": "4",
-              "isStep": true,
-              "inputs": [
-                {
-                  "name": "input_seq",
-                  "type": "int",
-                  "value": ""
-                }
-              ],
-              "OutputParams": {
-                "output_sequence": ""
-              },
-              "description": "BLAST finds regions of similarity between biological sequences. Uses the DDBJ’s UniProt BLAST service that forms part of its Web API for Bioinformatics pages."
-            }
+            // {
+            //   "Id": "S002",
+            //   "text": "FASTA",
+            //   "precedence": "4",
+            //   "isStep": true,
+            //   "inputs": [
+            //     {
+            //       "name": "db",
+            //       "type": "db",
+            //       "value": ""
+            //     },
+            //     {
+            //       "name": "retmode",
+            //       "type": "text",
+            //       "value": ""
+            //     }
+            //   ],
+            //   "OutputParams": {
+            //     "output": ""
+            //   },
+            //   "description": "FASTA aligns DNA and protein sequences"
+            // },
+            // {
+            //   "Id": "S003",
+            //   "text": "GBSeq",
+            //   "precedence": "4",
+            //   "isStep": true,
+            //   "inputs": [
+            //     {
+            //       "name": "db",
+            //       "type": "db",
+            //       "value": ""
+            //     },
+            //     {
+            //       "name": "retmode",
+            //       "type": "text",
+            //       "value": ""
+            //     }
+            //   ],
+            //   "OutputParams": {
+            //     "output": ""
+            //   },
+            //   "description": "GBSeq represents the elements in a GenBank style report of a sequence with some small additions to structure and support for protein (GenPept) versions of GenBank format as seen in Entrez."
+            // },
+            // {
+            //   "Id": "S004",
+            //   "text": "blast_ddbj",
+            //   "precedence": "4",
+            //   "isStep": true,
+            //   "inputs": [
+            //     {
+            //       "name": "query_seq",
+            //       "type": "seq",
+            //       "value": ""
+            //     },
+            //     {
+            //       "name": "database",
+            //       "type": "db",
+            //       "value": ""
+            //     },
+            //     {
+            //       "name": "program",
+            //       "type": "text",
+            //       "value": ""
+            //     }
+            //   ],
+            //   "OutputParams": {
+            //     "text_blast_out": ""
+            //   },
+            //   "description": "BLAST finds regions of similarity between biological sequences. Uses the DDBJ’s UniProt BLAST service that forms part of its Web API for Bioinformatics pages."
+            // },
+            // {
+            //   "Id": "S005",
+            //   "text": "blast simplifier",
+            //   "precedence": "4",
+            //   "isStep": true,
+            //   "inputs": [
+            //     {
+            //       "name": "gi_number",
+            //       "type": "int",
+            //       "value": ""
+            //     },
+            //     {
+            //       "name": "input_seq",
+            //       "type": "seq",
+            //       "value": ""
+            //     }
+            //   ],
+            //   "OutputParams": {
+            //     "simplified_report": ""
+            //   },
+            //   "description": "BLAST finds regions of similarity between biological sequences. Uses the DDBJ’s UniProt BLAST service that forms part of its Web API for Bioinformatics pages."
+            // },
+            // {
+            //   "Id": "S006",
+            //   "text": "Get_Protein_FASTA",
+            //   "precedence": "4",
+            //   "isStep": true,
+            //   "inputs": [
+            //     {
+            //       "name": "input_seq",
+            //       "type": "int",
+            //       "value": ""
+            //     }
+            //   ],
+            //   "OutputParams": {
+            //     "output_sequence": ""
+            //   },
+            //   "description": "BLAST finds regions of similarity between biological sequences. Uses the DDBJ’s UniProt BLAST service that forms part of its Web API for Bioinformatics pages."
+            // }
           ],
           "description": "List of web services offered by ncbi"
         },
