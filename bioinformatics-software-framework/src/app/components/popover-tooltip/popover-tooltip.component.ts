a753451c8e68a4ebba42cf094d38761832a6d8eb
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2017-11-09 22:38:26 +0530

    Add popover tooltips to Add and Execute buttons

---
+++ bioinformatics-software-framework/src/app/components/popover-tooltip/popover-tooltip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popover-tooltip',
+  templateUrl: './popover-tooltip.component.html',
+  styleUrls: ['./popover-tooltip.component.css']
+})
+export class PopoverTooltipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
