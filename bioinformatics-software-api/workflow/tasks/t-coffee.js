3025c6222dec9b2e997deb2da5dc9a105dc794bd
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2018-09-11 09:46:45 +1000

    bug fix

--- bioinformatics-software-api/workflow/tasks/t-coffee.js
+++ bioinformatics-software-api/workflow/tasks/t-coffee.js
@@ -7,7 +7,7 @@ import WrapTCoffee from "../../python-wrappers/t-coffee/wrap-t-coffee";
 export default class TCoffee {
 
     execute(...taskParams) {
-        const wrap = new WrapTCoffee(taskParams[0]);
+        const wrap = new WrapTCoffee(taskParams[0].output);
 
         return wrap.exec();
     }
